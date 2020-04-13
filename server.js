46e14dd96ac18f276d92bcc88165c28d759d1a9c
AmalMAlmomani <amalalmomani19@gmail.com>
2020-04-14 00:14:31 +0300

    11

--- server.js
+++ server.js
@@ -2,27 +2,14 @@
 require('dotenv').config();
 const express = require('express');
 const superagent = require('superagent');
-const request = require('request');
-const pg = require('pg');
+// const request = require('request');
+// const pg = require('pg');
 const PORT = process.env.PORT || 4000;
 const app = express();
+app.use(express.static('./public'));
+app.use(express.urlencoded({extended:true}));
 app.use(express.json());
 app.set('view engine', 'ejs');
-app.use(express.urlencoded({extended:true}));
-app.use(express.static('./public'));
-
-// app.get('/hello',(req,res)=>{
-//     res.render('./pages/index');
-//   })
-// app.get('/', (req, res) => {
-//     res.render('pages/index');
-// });
-
-// app.get('/', (req, res) => {
-//     // console.log('this is what we are getting', req.query);
-//     res.render('pages/searches/show');
-    
-// });
 
 
 
@@ -30,15 +17,26 @@ app.use(express.static('./public'));
 //     res.render('pages/index');
 //   });
 
-app.get('/', (req, res) => {
-    res.render('./pages/searches/new', req.body);
-});
+// app.get('/hello', (req, res) => {
+//     res.render('pages/index');
+// });
 
-app.post('/form',(req,res) =>{
-    const enterBook = request.body.enter;
+
+app.get('/', (req, res) => {
+    res.render('./pages/index')
+  });
+  app.get('/searches/new', (req, res) => {
+    res.render('./pages/searches/new');
+  })
+//   app.get('/', (req, res) => {
+//     res.render('./pages/searches/new');
+//   })
+
+app.post('/searches',(req,res) =>{
+    const enterBook = req.body.enter;
     console.log(enterBook);
     
-    const radioType = request.body.radioType;
+    const radioType = req.body.radioType;
     let url; 
     if( radioType === 'title'){
         url = `https://www.googleapis.com/books/v1/volumes?q=${enterBook}`;
@@ -47,17 +45,16 @@ app.post('/form',(req,res) =>{
     }
     superagent.get(url)
     .then(bookData =>{
-        let bookSummaries = bookData.body.items.map( bookVal =>{
+        const bookSummaries = bookData.body.items.map( bookVal =>{
             return new Book(bookVal);
         })
-        response.render('./pages/searches/show' , {search:bookSummaries});
+        res.render('./pages/searches/show',{search:bookSummaries});
     })
     .catch(err => { errorHandler(err,req,res);
     });
 
 
 })
-
 
 
 function Book(books){
