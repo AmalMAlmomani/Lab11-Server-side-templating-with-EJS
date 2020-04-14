2181464db5b17f1dee5efade4ef844125647aa14
AmalMAlmomani <amalalmomani19@gmail.com>
2020-04-14 05:03:16 +0300

    lab12

--- server.js
+++ server.js
@@ -1,76 +1,106 @@
 'use strict';
 require('dotenv').config();
 const express = require('express');
+const pg = require('pg');
+const cors = require('cors');
 const superagent = require('superagent');
+
 // const request = require('request');
-// const pg = require('pg');
 const PORT = process.env.PORT || 4000;
 const app = express();
+//middlewares
+app.use(express.urlencoded({ extended: true }));
 app.use(express.static('./public'));
-app.use(express.urlencoded({extended:true}));
-app.use(express.json());
+//view engine
 app.set('view engine', 'ejs');
+app.use(express.json());
+app.use(cors());
+
+const client = new pg.Client(process.env.DATABASE_URL);
+client.on('error', err => console.error(err));
+
+
+// Route
+
+app.get('/', getBooks);
+
+app.get('/book/:id', getOneBook);
+
+app.get('/searches/new', formRender);
+app.post('/searches', formNew );
+app.use('*', notFoundHandler);
+app.use(errorHandler);
 
 
 
-// app.get('/', (req, res) => {
-//     res.render('pages/index');
-//   });
+function getBooks(req, res) {
+    const SQL = 'SELECT * FROM books;';
+    client
+        .query(SQL)
+        .then(results => {
+            res.render('pages/index', { book: results.rows });
+        })
+        .catch((err) => {
+            errorHandler(err, req, res);
+        });
 
-// app.get('/hello', (req, res) => {
-//     res.render('pages/index');
-// });
+}
+function getOneBook(req, res) {
+    const SQL = 'SELECT * FROM books WHERE id=$1;';
+    const values = [req.params.id];
+    return client
+        .query(SQL, values)
+        .then(results => {
+            res.render('./pages/books/detail', { book: results.rows[0] });
+        })
+        .catch((err) => {
+            errorHandler(err, req, res);
+        });
+}
 
 
-app.get('/', (req, res) => {
-    res.render('./pages/index')
-  });
-  app.get('/searches/new', (req, res) => {
+function formRender(req, res) {
     res.render('./pages/searches/new');
-  })
+}
 //   app.get('/', (req, res) => {
 //     res.render('./pages/searches/new');
 //   })
 
-app.post('/searches',(req,res) =>{
+function formNew(req, res){
     const enterBook = req.body.enter;
-    console.log(enterBook);
-    
     const radioType = req.body.radioType;
-    let url; 
-    if( radioType === 'title'){
-        url = `https://www.googleapis.com/books/v1/volumes?q=${enterBook}`;
-    }else if( radioType === 'author'){
-        url = `https://www.googleapis.com/books/v1/volumes?q=${enterBook}`;
-    }
+    let url = `https://www.googleapis.com/books/v1/volumes?q=${enterBook}&in${radioType}=${enterBook}`;
     superagent.get(url)
-    .then(bookData =>{
-        const bookSummaries = bookData.body.items.map( bookVal =>{
-            return new Book(bookVal);
+        .then(bookData => {
+            const bookSummaries = bookData.body.items.map(bookVal => {
+                return new Book(bookVal);
+            })
+            res.render('./pages/searches/show', { search: bookSummaries });
         })
-        res.render('./pages/searches/show',{search:bookSummaries});
-    })
-    .catch(err => { errorHandler(err,req,res);
-    });
+        .catch(err => {
+            errorHandler(err, req, res);
+        });
 
 
-})
-
-
-function Book(books){
-    this.title = books.volumeInfo.title;
-    this.author = books.volumeInfo.authors[0];
-    this.img = books.volumeInfo.imageLinks.smallThumbnail;
-    this.description = books.volumeInfo.description;
 }
 
-function errorHandler (err,req,res){
-    res.status(500).send(err);
-  }
-  
 
-app.use('*', (request, response) => {
-    response.status(404).send('Page not found');
+function Book(books) {
+    this.title = books.volumeInfo.title ? books.volumeInfo.title : "Defult Title";
+    this.author = books.volumeInfo.authors[0] ? books.volumeInfo.authors[0] : "Unknown Authors";
+    this.image_url = books.volumeInfo.imageLinks.smallThumbnail ? books.volumeInfo.imageLinks.smallThumbnail : "No Image Found";
+    this.isbn = books.volumeInfo.industryIdentifiers ? books.volumeInfo.industryIdentifiers[0].identifier : "No ISBN Found";
+    this.description = books.volumeInfo.description ? books.volumeInfo.description : "No Description Found";
+}
+
+
+
+function notFoundHandler(req, res) {
+    res.status(404).send('PAGE NOT FOUND');
+}
+function errorHandler(err, req, res) {
+    res.status(500).render('pages/error', { error: err });
+}
+client.connect().then(() => {
+    app.listen(PORT, () => console.log('up on', PORT));
 });
-
-app.listen(PORT, () => console.log(`up and running on port ${PORT}`));
